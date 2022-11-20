9bfc0e55cfbc2b4faa84103784f52aaaa9b21d7e
Norbert Jankowski <norbert.jankowski.akademia@gmail.com>
2022-11-20 03:13:30 +0100

    Dodanie README.md, stworzenie/edycja gameActivity, poprawy estetyczne

--- app/src/main/java/com/example/atomilion/GameActivity.kt
+++ app/src/main/java/com/example/atomilion/GameActivity.kt
@@ -3,33 +3,113 @@ package com.example.atomilion
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.opencsv.CSVReader
 import java.io.File
 import java.io.FileReader
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets
+import com.example.atomilion.databinding.ActivityGameBinding
 
-class GameActivity : AppCompatActivity()  {
+public class GameActivity : AppCompatActivity()  {
+
+    private lateinit var binding: ActivityGameBinding
+    private var rightAnswer: String? = null
+    private var rightAnswerCount = 0
+    private var quizCount = 1
+    private val QUIZ_COUNT = 5
+
+    private val quizData = mutableListOf(
+        // pytanie, odp1, odp2, odp3, odp4
+        mutableListOf("Antracyt jest alotropową odmianą...","Węgla","Kadmu","Złota","Glinu"),
+        mutableListOf("Radioaktywny jest...","Uran","Hel","Azot","Argon"),
+        mutableListOf("Metalem ciężkim jest...","Rtęć","Żelazo","Srebro","Magnez"),
+        mutableListOf("Skóra się robi niebieska od nadmiaru...","Srebra","Krzemu","Złota","Niklu"),
+        mutableListOf("Czy Pan Wojciech jest super?","Oczywiście, że tak!","Nie","Pierwsze słyszę","Pogadajmy o czymś innym"),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        supportActionBar?.hide()
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val buttonA = findViewById<Button>(R.id.Ans_A)
-        val buttonB = findViewById<Button>(R.id.Ans_B)
-        val buttonC = findViewById<Button>(R.id.Ans_C)
-        val buttonD = findViewById<Button>(R.id.Ans_D)
+        quizData.shuffle()
 
-        buttonA.text="OdpA"
+        showNextQuiz()
+    }
 
-        val streamReader = InputStreamReader(assets.open("questions.csv"))
-        val reader = CSVReader(streamReader)
+    fun showNextQuiz() {
+        // Odświeżanie countLabel
+        binding.countLabel.text = getString(R.string.count_label, quizCount)
 
-        var line = reader.readNext()
-        for (value in line){
-            println(value)
+        val quiz = quizData[0]
+
+        // ustawienie pytania i poprawnej odpowiedzi
+
+        binding.questionLabel.text = quiz[0]
+        rightAnswer = quiz[1]
+
+        //Wyrzucenie wyrazu "pytanie" z quizu
+        quiz.removeAt(0)
+
+        // Losowanie odpowiedzi i wyborów.
+        quiz.shuffle()
+
+
+        // Ustawienie wyborów.
+        binding.answerBtn1.text = quiz[0]
+        binding.answerBtn2.text = quiz[1]
+        binding.answerBtn3.text = quiz[2]
+        binding.answerBtn4.text = quiz[3]
+
+        // Usunięcie quizu z quizData
+
+        quizData.removeAt(0)
+
+    }
+
+    fun checkAnswer(view: View) {
+        // Wciskanie przycisku
+        val answerBtn: Button = findViewById(view.id)
+        val btnText = answerBtn.text.toString()
+
+        val alertTitle: String
+                if (btnText == rightAnswer) {
+                    // Correct
+                    alertTitle = "Dobra odpowiedź!"
+                    rightAnswerCount++
+                } else {
+                    // Wrong
+                    alertTitle = "Niestety..."
+                }
+
+        // Komunikat
+        AlertDialog.Builder(this)
+            .setTitle(alertTitle)
+            .setMessage("Prawidłowa odpowiedź: $rightAnswer")
+            .setPositiveButton("OK") {dialogInterface, i ->
+                checkQuizCount()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    fun checkQuizCount() {
+        if (quizCount == QUIZ_COUNT) {
+            // Show Result
+            val intent = Intent(this@GameActivity, ResultActivity::class.java)
+            intent.putExtra("RIGHT_ANSWER_COUNT", rightAnswerCount)
+            startActivity(intent)
+
+        } else {
+            quizCount++
+            showNextQuiz()
         }
     }
 }
